1c7e2bbd7470d1e2cddcd82f3984654c36ce0b2c
Alexander Storchak <alexander.s.storchak@gmail.com>
2025-08-23 17:54:08 +0300

    Добавил тесты для helper.

--- internal/service/helper.go
+++ internal/service/helper.go
@@ -5,15 +5,22 @@ import (
 	"time"
 )
 
+const (
+	nanoSecFmt  = "%d ns"
+	microSecFmt = "%.2f μs"
+	milliSecFmt = "%.2f ms"
+	secFmt      = "%.2f s"
+)
+
 func FormatDuration(d time.Duration) string {
 	switch {
 	case d < time.Microsecond:
-		return fmt.Sprintf("%d ns", d.Nanoseconds())
+		return fmt.Sprintf(nanoSecFmt, d.Nanoseconds())
 	case d < time.Millisecond:
-		return fmt.Sprintf("%.2f μs", float64(d.Microseconds()))
+		return fmt.Sprintf(microSecFmt, float64(d.Microseconds()))
 	case d < time.Second:
-		return fmt.Sprintf("%.2f ms", float64(d.Milliseconds()))
+		return fmt.Sprintf(milliSecFmt, float64(d.Milliseconds()))
 	default:
-		return fmt.Sprintf("%.2f s", d.Seconds())
+		return fmt.Sprintf(secFmt, d.Seconds())
 	}
 }
