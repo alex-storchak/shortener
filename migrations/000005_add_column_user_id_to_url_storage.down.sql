ba1c31cbc51ec4806f4e0fdf88aff10f2c558e8b
Alexander Storchak <alexander.s.storchak@gmail.com>
2025-10-01 14:48:09 +0300

    Инкремент 14. wipedb fix (потому что оно не умеет удалять внешние ключи...)

--- migrations/000005_add_column_user_id_to_url_storage.down.sql
+++ migrations/000005_add_column_user_id_to_url_storage.down.sql
@@ -4,7 +4,8 @@ DROP INDEX IF EXISTS idx_url_storage_original_url_user_id;
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_url_storage_original_url ON public.url_storage (original_url);
 
-ALTER TABLE url_storage DROP CONSTRAINT IF EXISTS fk_url_storage_user_id;
+-- https://github.com/Yandex-Practicum/go-autotests/pull/89/files
+-- ALTER TABLE url_storage DROP CONSTRAINT IF EXISTS fk_url_storage_user_id;
 
 DROP INDEX IF EXISTS idx_url_storage_user_id;
 
--- migrations/000005_add_column_user_id_to_url_storage.up.sql
+++ migrations/000005_add_column_user_id_to_url_storage.up.sql
@@ -4,7 +4,8 @@ ALTER TABLE url_storage ADD COLUMN IF NOT EXISTS user_id INTEGER;
 
 CREATE INDEX IF NOT EXISTS idx_url_storage_user_id ON url_storage(user_id);
 
-ALTER TABLE url_storage ADD CONSTRAINT fk_url_storage_user_id FOREIGN KEY (user_id) REFERENCES auth_user(id);
+-- https://github.com/Yandex-Practicum/go-autotests/pull/89/files
+-- ALTER TABLE url_storage ADD CONSTRAINT fk_url_storage_user_id FOREIGN KEY (user_id) REFERENCES auth_user(id);
 
 DROP INDEX IF EXISTS public.idx_url_storage_original_url;
 
